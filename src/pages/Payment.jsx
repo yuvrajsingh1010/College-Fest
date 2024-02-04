8521b8123851b13983ebee37aa10389ea8a6f4f2
niranjan-official <niranjansabarianth1521@gmail.com>
2024-02-04 12:24:50 +0530

    payment page done

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -3,7 +3,65 @@ import React from "react";
 const Payment = () => {
   return (
     <section id="payment">
-      
+      <div className="w-full h-max lg:min-h-screen flex flex-col items-center text-white p-6">
+        <h1 className=" flex md:flex-col poppins-semibold text-3xl md:text-4xl text-center pb-4">
+          <span>
+            Welcome to our Festival <br className="hidden md:block" /> Payment
+            Page
+          </span>
+        </h1>
+        <p className="poppins-medium">
+          <span className="text-secondary">" </span> Your support plays a
+          crucial role in making our event a grand success
+          <span className="text-secondary"> "</span>
+        </p>
+        <div className="flex flex-col md:flex-row ">
+          <div className="md:w-1/2 flex flex-col text-white p-4 items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6 text-secondary"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M16.5 18.75h-9m9 0a3 3 0 0 1 3 3h-15a3 3 0 0 1 3-3m9 0v-3.375c0-.621-.503-1.125-1.125-1.125h-.871M7.5 18.75v-3.375c0-.621.504-1.125 1.125-1.125h.872m5.007 0H9.497m5.007 0a7.454 7.454 0 0 1-.982-3.172M9.497 14.25a7.454 7.454 0 0 0 .981-3.172M5.25 4.236c-.982.143-1.954.317-2.916.52A6.003 6.003 0 0 0 7.73 9.728M5.25 4.236V4.5c0 2.108.966 3.99 2.48 5.228M5.25 4.236V2.721C7.456 2.41 9.71 2.25 12 2.25c2.291 0 4.545.16 6.75.47v1.516M7.73 9.728a6.726 6.726 0 0 0 2.748 1.35m8.272-6.842V4.5c0 2.108-.966 3.99-2.48 5.228m2.48-5.492a46.32 46.32 0 0 1 2.916.52 6.003 6.003 0 0 1-5.395 4.972m0 0a6.726 6.726 0 0 1-2.749 1.35m0 0a6.772 6.772 0 0 1-3.044 0"
+              />
+            </svg>
+            <h2 className="poppins-bold py-2">Purpose of Payment</h2>
+            <p className="poppins-regular text-center">
+              Your contributions directly contribute to enhancing the overall
+              experience for students and attendees, ensuring memorable
+              performances, workshops, and activities.
+            </p>
+          </div>
+          <div className="md:w-1/2 flex flex-col text-white p-4 items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6 text-secondary"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M16.5 18.75h-9m9 0a3 3 0 0 1 3 3h-15a3 3 0 0 1 3-3m9 0v-3.375c0-.621-.503-1.125-1.125-1.125h-.871M7.5 18.75v-3.375c0-.621.504-1.125 1.125-1.125h.872m5.007 0H9.497m5.007 0a7.454 7.454 0 0 1-.982-3.172M9.497 14.25a7.454 7.454 0 0 0 .981-3.172M5.25 4.236c-.982.143-1.954.317-2.916.52A6.003 6.003 0 0 0 7.73 9.728M5.25 4.236V4.5c0 2.108.966 3.99 2.48 5.228M5.25 4.236V2.721C7.456 2.41 9.71 2.25 12 2.25c2.291 0 4.545.16 6.75.47v1.516M7.73 9.728a6.726 6.726 0 0 0 2.748 1.35m8.272-6.842V4.5c0 2.108-.966 3.99-2.48 5.228m2.48-5.492a46.32 46.32 0 0 1 2.916.52 6.003 6.003 0 0 1-5.395 4.972m0 0a6.726 6.726 0 0 1-2.749 1.35m0 0a6.772 6.772 0 0 1-3.044 0"
+              />
+            </svg>
+            <h2 className="poppins-bold py-2">Why Payment?</h2>
+            <p className="poppins-regular text-center">
+              By supporting our event, you are investing in the growth and
+              development of young talents, fostering creativity, and promoting
+              a vibrant cultural exchange.
+            </p>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
