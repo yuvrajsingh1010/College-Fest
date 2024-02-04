937adb08d76df335210a8354261214c96c9c0058
niranjan-official <niranjansabarianth1521@gmail.com>
2024-02-04 10:51:35 +0530

    tailwinf.config edited

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { Home } from "lucide-react"
+
 import Navbar from "./components/Navbar"
 import About from "./pages/About"
 import Payment from "./pages/Payment"
 import Events from "./pages/Events"
 import Contact from "./pages/Contact"
 import Footer from "./components/Footer"
+import Home from "./pages/Home"
 
 function App() {
 
   return (
-    <div className="w-screen h-max bg-primary">
+    <div className="w-screen bg-primary">
       <Navbar/>
       <Home/>
       <About/>
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,12 +16,62 @@ module.exports = {
         "2xl": "1400px",
       },
     },
-    extend:{
+    extend: {
       colors: {
-        'primary' : '#070415',
-        'secondary': '#ba3375',
-      }
-    }
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "#070415",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "#ba3375",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
   },
   plugins: [require("tailwindcss-animate")],
 }
