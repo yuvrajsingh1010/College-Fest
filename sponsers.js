bcb7f19866008e7437b5efad2a7a0c962ae97446
niranjan-official <niranjansabarinath1521@gmail.com>
2024-02-21 23:54:32 +0530

    sponsers added

---
+++ sponsers.js
@@ -0,0 +1,46 @@
+export const sponsers = [
+    {
+        name: 'jayalakshmi',
+        src: './sponsers/jayalakshmi.PNG'
+    },
+    {
+        name: 'josco',
+        src: './sponsers/josco.PNG'
+    },
+    {
+        name: 'kallarackal',
+        src: './sponsers/kallarackal.PNG'
+    },
+    {
+        name: 'kip',
+        src: './sponsers/kip.jpeg'
+    },
+    {
+        name: 'redmatrix',
+        src: './sponsers/redmatrix.jpg'
+    },
+    {
+        name: 'usha',
+        src: './sponsers/usha.jpeg'
+    },
+    {
+        name: 'dreamflower',
+        src: './sponsers/dreamflower.png'
+    },
+    {
+        name: 'qmark',
+        src: './sponsers/qmark.png'
+    },
+    {
+        name: 'wealthmetrics',
+        src: './sponsers/wealthmetrics.jpeg'
+    },
+    {
+        name: 'strategist',
+        src: './sponsers/strategist.png'
+    },
+]
+export const mainSponser = {
+    name: 'reddot',
+    src: './sponsers/reddot.PNG'
+}
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../assets/logo.png"
+import { sponsers } from "../../sponsers";
+
+import { mainSponser } from "../../sponsers";
+
 
 const Footer = () => {
   return (
-    <div className="w-full flex flex-col md:flex-row justify-center md:justify-between items-center p-4 py-7 gap-3">
-      <div className="flex flex-col md:flex-row justify-center items-center gap">
+    <div className="w-full flex flex-col justify-center items-center p-4 py-7 gap-3">
+      <div className="flex flex-col justify-center items-center gap">
         <img src={logo} className="w-20 h-auto" />
         <h1 className="poppins-bold text-white text-xl">
           SHIKHAR'<span className="text-secondary">24</span>
         </h1>
       </div>
-      <hr className="w-full text-white border-white md:hidden" />
+      <div className="w-full flex flex-col justify-center items-center">
+        <span className="w-full flex items-center justify-center gap-4">
+        <hr className="w-40 text-white border-white" />
+        <h1 className="text-secondary text-3xl poppins-bold text-center">Powered By</h1>
+        <hr className="w-40 text-white border-white" />
+        </span>
+        <div className="py-4 flex flex-col justify-center items-center">
+          <img src={mainSponser.src} className="p-4 w-full sm:w-1/2 md:w-1/4 h-auto" alt="" />
+          <div className="flex flex-wrap justify-center">
+          {
+            sponsers.map((obj,key)=>{
+              return (
+                <img key={key} src={obj.src} alt={obj.name} className="p-4 h-24 w-auto" />
+              )
+            })
+          }
+          </div>
+        </div>
+      </div>
+      <hr className="w-full text-white border-white" />
       <p className="text-white poppins-regular text-sm">
         © Shikhar 2024. All right reserved.
       </p>
